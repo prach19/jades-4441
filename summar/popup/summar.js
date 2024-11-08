ef9ef5282ce7a9b4d2571dddab0278aa1c9f3bce
prach19 <prachi19@my.yorku.ca>
2024-11-08 13:58:33 -0500

    improving button functionality, added policy.json

--- summar/popup/summar.js
+++ summar/popup/summar.js
@@ -1,31 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
     // references to buttons
-    let collection = document.getElementById("collect-btn");
-    let rights = document.getElementById("rights-btn");
-    let retention = document.getElementById("ret-btn");
-    let storage = document.getElementById("storage-btn");
+    let collection = document.getElementById("collect");
+    let rights = document.getElementById("rights");
+    let retention = document.getElementById("retention");
+    let storage = document.getElementById("storage");
 
     //reference to content box
     let policyContent = document.getElementById("content");
+    let amazonData = {};
 
-    //functionality for collection button
+    fetch(chrome.runtime.getURL("policy.json"))
+    .then((response) => response.json())
+    .then((data) => {
+      // Store the first object in amazonData for easy access
+      amazonData = data[0];
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
+
+    //functionality for rights button
     collection.addEventListener("click", function () {
-        policyContent.textContent = "collection policy";
+        policyContent.textContent = amazonData.collection;
     });
 
     //functionality for rights button
     rights.addEventListener("click", function () {
-        policyContent.textContent = "rights policy";
+        policyContent.textContent = amazonData.rights;
     });
 
     // functionality for retention button
     retention.addEventListener("click", function () {
-
-        policyContent.textContent = "retention policy";
+        policyContent.textContent = amazonData.retention;
     });
 
     // functionality for storage button
     storage.addEventListener("click", function () {
-        policyContent.textContent = "storage policy";
+        policyContent.textContent = amazonData.storage;
     });
+
+    
+
+
 });
